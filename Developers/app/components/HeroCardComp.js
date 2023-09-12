b6160db0a9a28000ff37536f4c57010ac6072910
Edidiong <etokedidionge@gmail.com>
2023-09-12 11:51:45 +0100

    remove font

--- Developers/app/components/HeroCardComp.js
+++ Developers/app/components/HeroCardComp.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function HeroCardComp({lightHeaderText, boldHeaderText, description}) {
   return (
-    <div className="bg-white w-[560px] p-4 font-lexend">
+    <div className="bg-white w-[560px] p-4">
       <p className="capitalize text-6xl">{lightHeaderText} </p>
       <p className="text-6xl font-bold mt-1 mb-2 capitalize">{boldHeaderText}</p>
-      <p className="text-lg font-extralight ">{description}</p>
+      <p className="text-lg">{description}</p>
     </div>
   );
 }
