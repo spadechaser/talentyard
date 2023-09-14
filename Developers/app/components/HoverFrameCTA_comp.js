191284485dbc70ab54b6a54c4dca2dd3a095911b
spadechaser <queenmaker@geniuskingsley.dev>
2023-09-14 10:21:23 -0400

    use hero-icon

--- Developers/app/components/HoverFrameCTA_comp.js
+++ Developers/app/components/HoverFrameCTA_comp.js
@@ -1,44 +1,50 @@
 "use client";
 
-import Image from "next/image";
+import {
+  CommandLineIcon,
+  PencilIcon,
+  RectangleGroupIcon,
+} from "@heroicons/react/24/outline";
 
 const frame = [
   {
     id: 1,
-    src: "/assets/images/icons/hover-frame-cta1.png",
+    src: <CommandLineIcon />,
     height: 42,
     width: 42,
   },
   {
     id: 2,
-    src: "/assets/images/icons/hover-frame-cta2.png",
+    src: <PencilIcon />,
     height: 42,
     width: 42,
   },
   {
     id: 3,
-    src: "/assets/images/icons/hover-frame-cta3.png",
+    src: <RectangleGroupIcon />,
     height: 42,
     width: 42,
   },
 ];
+
 export default function HoverFrameCTA() {
   return (
-    <div className="relative flex flex-col mx-auto rounded-3xl w-[360px] h-[417px] drop-shadow-2xl hover:opacity-70">
+    <div className="flex flex-col mx-auto fixed bottom-16 right-16 rounded-3xl w-[360px] h-[417px] drop-shadow-2xl hover:opacity-70">
       <div className="relative [background:linear-gradient(180deg,_#4d90db,_#0066c5_23.27%,_#004b9b)] rounded-t-3xl flex-1">
         <h1 className="mt-[51px] text-center text-slate-100 text-[34px] font-bold font-lexend my-auto">
           Join Talentyard
         </h1>
-        <figure className="mt-[28px] flex items-center justify-center space-x-[34px] object-contain pointer-events-none mb-[40px]">
+        <div className="mt-[28px] flex items-center justify-center space-x-[34px] object-contain mb-[40px]">
           {frame.map((value) => (
-            <Image
-              src={value.src}
-              height={value.height}
-              width={value.width}
-              alt="icon"
-            />
+            <figure
+              style={{ height: `${value.height}px`, width: `${value.width}px` }}
+              className="text-slate-100"
+              key={value.id}
+            >
+              {value.src}
+            </figure>
           ))}
-        </figure>
+        </div>
       </div>
       <div className="bg-slate-100 flex-1 rounded-b-3xl">
         <div className=" flex items-center justify-center mx-5 mt-[37px]">
