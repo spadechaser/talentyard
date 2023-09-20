8060e724dc75a30908cd7addd9698560b7e29994
Edidiong <etokedidionge@gmail.com>
2023-09-20 18:55:43 +0100

    remove width and margin top

--- Developers/app/components/onboardTechies/onboardingQuoteCard.js
+++ Developers/app/components/onboardTechies/onboardingQuoteCard.js
@@ -1,16 +1,13 @@
 import React from "react";
 
-
 function OnboardingQuoteCard() {
   return (
-    <div className="max-w-[22rem] md:max-w-md mx-auto px-4 pt-4 pb-8 bg-dark rounded-3xl">
+    <div className="px-4 pt-4 pb-8 bg-dark rounded-3xl space-y-4">
       <p className="text-white text-xl md:text-3xl font-semibold">
         TalentYard is a <span className="text-accentSecondary">playground</span>{" "}
         for innovative talents
-        <div className="flex space-x-2 my-5">
-          <div className="text-7xl md:text-[92px] text-secondary -mt-2">
-           "
-          </div>
+        <div className="flex space-x-2">
+          <div className="text-7xl md:text-[92px] text-secondary -mt-2">"</div>
           <p className="font-light text-white text-sm md:text-xl">
             The goal of TalentYard is not just about connecting talents with
             work opportunities, it’s about fostering a community of innovative
