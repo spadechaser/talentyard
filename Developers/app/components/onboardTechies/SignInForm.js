0f66430dba27a08140a3756e13861d039f5eb8b7
Bright Lazarus <lazbright1@gmail.com>
2023-09-26 19:47:11 +0100

    Update SignInForm.js
    Resolved all the comments and issues

--- Developers/app/components/onboardTechies/SignInForm.js
+++ Developers/app/components/onboardTechies/SignInForm.js
@@ -3,21 +3,33 @@
 import React, { useState } from "react";
 
 function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
   const [error, setError] = useState(null);
 
   const handleSignIn = async (e) => {
     e.preventDefault();
 
     try {
-    } catch (error) {}
+    } catch (error) {
+      setError(error.message);
+    }
   };
 
   const handleForgotPassword = () => {};
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
-    <div className="w-full max-w-xs sm:max-w-[608px] mx-auto mt-10 space-y-[33px]">
+    <div className="space-y-6">
       <h2 className="text-black text-[32px] font-normal leading-[41.92px] tracking-wide">
         Welcome back
       </h2>
@@ -25,16 +37,19 @@ function SignIn() {
         Sign in to get access to your work reports, status updates, profile and
         invoices.
       </p>
-      <form onSubmit={handleSignIn} className="space-y-[31px]">
+      <form
+        onSubmit={handleSignIn}
+        className="space-y-[31px] w-full max-w-[608px]"
+      >
         <div>
           <input
             type="email"
             id="email"
             name="email"
-            value={email}
+            value={formData.email}
             placeholder="Email"
             required
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={handleInputChange}
             className="py-2 pl-4 focus:outline-none bg-white rounded-[7px] w-full m-auto border border-gray-200 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-primary sm:text-sm sm:leading-6 p-3 placeholder:font-light placeholder:text-black leading-normal tracking-tight"
           />
         </div>
@@ -44,22 +59,24 @@ function SignIn() {
               type="password"
               id="password"
               name="password"
-              value={password}
+              value={formData.password}
               placeholder="Password"
               required
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handleInputChange}
               className="py-2 pl-4 focus:outline-none bg-white rounded-[7px] w-full m-auto border border-gray-200 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-primary sm:text-sm sm:leading-6 p-3 placeholder:font-light placeholder:text-black leading-normal tracking-tight"
             />
-            <p
-              onClick={handleForgotPassword}
-              className="text-sky-800 text-right text-sm font-normal leading-[18.34px] tracking-tight cursor-pointer hover:underline"
-            >
-              Forgot Password
-            </p>
+            <div className="w-full flex justify-end">
+              <a
+                onClick={handleForgotPassword}
+                className="text-sky-800 text-right text-sm font-normal leading-[18.34px] tracking-tight cursor-pointer hover:underline"
+              >
+                Forgot Password
+              </a>
+            </div>
           </div>
           <button
             type="submit"
-            className="w-full max-w-[227px] py-3 bg-primary rounded-xl hover:shadow-lg shadow-accentSecondary/30 transition delay-150 ease-in-out hover:scale-105 duration-300 hover:bg-dark text-white text-lg font-semibold"
+            className="w-full sm:max-w-[227px] py-3 bg-primary rounded-xl transition delay-150 ease-in-out hover:scale-105 duration-300 hover:bg-dark text-white text-lg font-semibold"
           >
             Login
           </button>
